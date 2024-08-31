6077c0b6789e00f0d413521ee799bcec042995e7
erichobson <sweeter-01hydras@icloud.com>
2024-08-31 17:46:50 -0400

    Updated mini.lua

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -5,10 +5,5 @@ return {
         require('mini.cursorword').setup()
         require('mini.pairs').setup()
         require('mini.comment').setup()
-        -- require('mini.indentscope').setup()
-        -- require('mini.move').setup()
-        -- require('mini.pick').setup()
-        -- require('mini.splitjoin').setup()
-        -- require('mini.surround').setup()
     end,
 }
