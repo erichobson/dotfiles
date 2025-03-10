17a78bc03f3696ea2f94ab28a3341d7011f343ed
Eric Hobson <sweeter-01hydras@icloud.com>
2025-03-09 22:55:38 -0400

    Updated sketchybar configuration

---
+++ sketchybar/themes/evergarden.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Color Palette
+export BLACK=0xff171C1F       # $crust
+export WHITE=0xffD5D6C8       # $text
+export RED=0xffE67E80         # $red
+export GREEN=0xffB2C98F       # $green
+export BLUE=0xff9BB5CF        # $blue
+export YELLOW=0xffDBBC7F      # $yellow
+export ORANGE=0xffE69875      # $peach
+export MAGENTA=0xffD6A0D1     # $mauve
+export GREY=0xff5E6C70        # $overlay0
+export TRANSPARENT=0x00000000
+
+# General bar colors
+export BAR_COLOR=0xa0232A2E
+export ICON_COLOR=$WHITE
+export LABEL_COLOR=$WHITE
+export BACKGROUND_1=0xff313C40
+export BACKGROUND_2=0xff3F4D52
+
+export POPUP_BACKGROUND_COLOR=0xff1C2225
+export POPUP_BORDER_COLOR=$WHITE
+
+export SHADOW_COLOR=$BLACK
